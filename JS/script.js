72371c4374766c38c7091728b25b6273fd7337d7
Petros Tsakonas <petros-tsakonas@hotmail.com>
2022-10-19 20:42:20 +0300

    UI creation and JS addition
    a) In the UI, the player is able to play the game by clicking on buttons rather than
    typing their answer in a prompt.
    b) Created three buttons, one for each selection. Added an event listener to the buttons that call the playRound function with the correct playerSelection every time a button is clicked.
    c) Added a div for displaying results and changed all of the console.logs into DOM methods.
    d) Displayed the running score, and announced a winner of the game once one player reaches 5 points.

---
+++ JS/script.js
@@ -0,0 +1,71 @@
+// Main JS Project
+
+const gameOptions = ["rock", "paper", "scissors"];
+
+let playerSelection, computerSelection, roundResult, winner,
+round = 1,
+playerScore = 0,
+computerScore = 0,
+resultText = document.querySelector('#round-result-text'),
+scoreText = document.querySelector('#score-text'),
+playerChoiceText = document.querySelector('#player-choice-text');
+
+const buttons = document.querySelectorAll('button');
+
+const displayResult = (roundResult, scoreResult) => {
+    resultText.textContent = roundResult;
+    scoreText.textContent = scoreResult;
+}
+
+const playRound = function(playerSelection) {
+    
+    computerSelection = gameOptions[Math.floor(Math.random() * gameOptions.length)];
+    
+    // Round Logic
+    if (playerSelection == computerSelection) roundResult = `Round ${round} is a draw!`;
+    
+    if (
+        playerSelection == "rock" && computerSelection == "paper" ||
+        playerSelection == "scissors" && computerSelection == "rock" ||
+        playerSelection == "paper" && computerSelection == "scissors"
+    ) {
+        computerScore++;
+
+        if (computerScore === 5) {
+            roundResult = "🔴 Computer wins the game! 😢";
+        } else {
+            roundResult = `Computer wins round ${round}! (${computerSelection} beats ${playerSelection})`;
+        }
+    }
+
+    if (
+        playerSelection == "paper" && computerSelection == "rock" ||
+        playerSelection == "rock" && computerSelection == "scissors" ||
+        playerSelection == "scissors" && computerSelection == "paper"
+    ) {
+        playerScore++;
+        if (playerScore === 5) {
+            roundResult = "👏 You win the game! 🐱‍🚀";
+        } else {
+            roundResult = `Player wins round ${round}! (${playerSelection} beats ${computerSelection})`;
+        }
+    } 
+    
+    scoreResult = `Player: ${playerScore} Computer: ${computerScore}`;
+
+    displayResult(roundResult, scoreResult);
+
+    round++;
+}
+
+buttons.forEach((button) => {
+
+    button.addEventListener('click', function(e) {
+        playerSelection = button.id;
+        playerChoiceText.textContent = `You chose ${playerSelection}!`;
+        playRound(playerSelection);
+    })
+
+})
+
+
